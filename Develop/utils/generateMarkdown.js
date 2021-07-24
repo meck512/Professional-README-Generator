911c3419dd036fae3b0f40477e7dcff8c3006872
meck512 <83519531+meck512@users.noreply.github.com>
2021-07-24 16:58:08 -0400

    Update generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,44 +1,34 @@
 
 function generateMarkdown(data) {
-  if (data.license === "MIT License") {
-    data.license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  }
-  if (data.license === "APACHE 2.0") {
-    data.license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  }
-  if (data.license === "GNU Public v3.0") {
-    data.license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  return `# ${data.title}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation-instructions)
+* [Usage Information](#usage-information)
+* [Contribution Guidelines](#contuibution-guidelines)
+* [License](#license) 
+* [Test instructions](#test-instructions)
+* [Questions](#questions)
+    
+## Description
+${data.description}
+## Installation Instructions
+${data.installation}
+## Usage Information
+${data.usage}
+## Contuibution Guidelines
+${data.contribution}
+## License
+[![license](https://img.shields.io/badge/license-${data.license}-blue)](https://shields.io)
+## Test Instructions
+${data.test}
+## Questions
+<p>Contact Information</p>
+### GitHub Profile
+[GitHub Profile](http://github.com/${data.username})
+### Email address
+[Email Address](${data.email})`
 }
 
-return `# ${data.title}
-
-    ## Table of Contents
-    - [Description](#Description)
-    - [Installation](#Installation-Instructions)
-    - [Usage Information](#Usage-Information)
-    - [Contribution Guidelines](#Contuibution-Guidelines)
-    - [License](#License)
-    - [Test instructions](#Test-Instructions) 
-    - [Questions](#Questions)
-    
-    ## Description
-    ${data.description}
-    ## Installation Instructions 
-    ${data.installation}
-    ## Usage Information 
-    ${data.usage}
-    ## Contuibution Guidelines
-    ${data.contribution}
-    ## License
-    ${data.license}
-    ## Test Instructions
-     ${data.test}
-    ## Questions
-    ### GitHub Profile
-    [GitHub Profile](http://github.com/${data.username})
-    ### Email address
-    [Email Address](${data.email})
-  `;
-  }
-
 module.exports = generateMarkdown;
+
